32abf2c6842c8402fcca7ae0a713315f3a2780ab
Bashar-Owainat <owainatbashar13@gmail.com>
2023-05-01 16:00:39 +0300

    added read me

---
+++ Covid_19_Application/Covid_19_Application/Migrations/20230501122736_AdddDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Covid_19_Application.Migrations
+{
+    public partial class AdddDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TotalConfirmed = table.Column<int>(type: "int", nullable: false),
+                    TotalDeaths = table.Column<int>(type: "int", nullable: false),
+                    TotalRecovered = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Records");
+        }
+    }
+}
---
+++ Covid_19_Application/Covid_19_Application/obj/Release/net5.0/PubTmp/EFSQLScripts/Covid_19_Application.Data.CovidDbContext.sql
@@ -0,0 +1,37 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230501122736_AdddDB')
+BEGIN
+    CREATE TABLE [Records] (
+        [ID] nvarchar(450) NOT NULL,
+        [Country] nvarchar(max) NULL,
+        [TotalConfirmed] int NOT NULL,
+        [TotalDeaths] int NOT NULL,
+        [TotalRecovered] int NOT NULL,
+        [Date] nvarchar(max) NULL,
+        CONSTRAINT [PK_Records] PRIMARY KEY ([ID])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230501122736_AdddDB')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230501122736_AdddDB', N'5.0.14');
+END;
+GO
+
+COMMIT;
+GO
+
